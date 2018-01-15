d333cd37d95e84593da090cc2da6e2bf47d56b6a
Liby Lee <liby99@icloud.com>
2018-01-15 15:10:22 -0800

    Initiated Camera

---
+++ src/extern/viewer/Camera.h
@@ -0,0 +1,14 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+
+class Camera {
+    private:
+        
+    public:
+        
+        Camera(glm::vec3 center);
+};
+
+#endif
---
+++ src/intern/viewer/Camera.cpp
@@ -0,0 +1,5 @@
+#include "viewer/Camera.h"
+
+Camera::Camera(glm::vec3 center) {
+    
+}
