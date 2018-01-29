54ac4f1813a6f7c304c4a2768e41b712dfaac3d0
Liby Lee <liby99@icloud.com>
2018-01-29 03:14:20 -0800

    Recovered project 1 test cases

--- test/project1/posed_horse.cpp
+++ test/project1/posed_horse.cpp
@@ -11,7 +11,6 @@ using namespace rotamina;
 int main() {
     Skeleton skel = SkeletonParser::loadSkeleton("../res/skeletons/horse.posed.skel");
     SkeletonViewer::createViewer(1440, 960, "CSE 169 - Project 1 - Horse", skel, [&](Viewer & v) {
-        // v.setCameraRotate(false);
         v.getCamera().setPosition(Eigen::Vector3f(3, 0, 0));
-    }, false);
+    });
 }
