b08701c6b98dcc9ba5b3c3318318ee5b2a292e4c
Liby Lee <liby99@icloud.com>
2018-01-29 21:49:33 -0800

    Add show/hide skin feature in skin viewer

--- src/extern/viewer/SkinViewer.h
+++ src/extern/viewer/SkinViewer.h
@@ -7,7 +7,11 @@
 namespace rotamina {
     class SkinViewer : public SkeletonViewer {
         protected:
+            
+            static const int CONTROL_PANEL_HEIGHT = 50;
+            
             rotamina::Skin * skin;
+            nanogui::Window * skinControlPanel;
         public:
             SkinViewer(int, int, std::string, Skin & skin);
             static void createViewer(int, int, std::string, Skin & skin, std::function<void(SkinViewer &)>);
--- src/intern/viewer/SkinViewer.cpp
+++ src/intern/viewer/SkinViewer.cpp
@@ -3,12 +3,49 @@
 using namespace rotamina;
 
 SkinViewer::SkinViewer(int w, int h, std::string name, Skin & skin) : SkeletonViewer(w, h, name, skin.getSkeleton()) {
+    
+    // Initiate basic variables
     this->skin = &skin;
     
-    scene->removeObject(0);
+    // Load scene
     scene->addObject(skin);
-    
     scene->getShader().initTwoLights();
+    
+    // Initiate visibility
+    this->skin->setHidden(false);
+    this->skin->getSkeleton().setHidden(true);
+    
+    // Initiate
+    using namespace nanogui;
+    
+    scene->setHeight(h - CONTROL_PANEL_HEIGHT - 2 * HEADER_HEIGHT - 2 * PADDING);
+    sceneViewer->setHeight(h - CONTROL_PANEL_HEIGHT - HEADER_HEIGHT);
+    
+    skinControlPanel = new Window(this, "Skin Control");
+    skinControlPanel->setPosition({ JOINTS_VIEWER_WIDTH, h - CONTROL_PANEL_HEIGHT - HEADER_HEIGHT });
+    skinControlPanel->setFixedSize({ w - JOINTS_VIEWER_WIDTH - JOINT_INFO_VIEWER_WIDTH, CONTROL_PANEL_HEIGHT + HEADER_HEIGHT });
+    GridLayout * layout = new GridLayout(Orientation::Horizontal, 2, Alignment::Middle, 10, 5);
+    skinControlPanel->setLayout(layout);
+    
+    Button * showSkinBtn = new Button(skinControlPanel, "Show Skin");
+    showSkinBtn->setFlags(Button::RadioButton);
+    showSkinBtn->setPushed(true);
+    showSkinBtn->setCallback([this] () {
+        this->skin->setHidden(false);
+        this->skin->getSkeleton().setHidden(true);
+    });
+    Button * showSkelBtn = new Button(skinControlPanel, "Show Skeleton");
+    showSkelBtn->setFlags(Button::RadioButton);
+    showSkelBtn->setCallback([this] () {
+        this->skin->setHidden(true);
+        this->skin->getSkeleton().setHidden(false);
+    });
+    
+    // Reset focus
+    sceneViewer->requestFocus();
+    
+    // performLayout
+    performLayout();
 }
 
 void SkinViewer::createViewer(int w, int h, std::string name, Skin & skin, std::function<void(SkinViewer &)> init) {
