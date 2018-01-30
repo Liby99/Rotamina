ed375985ba2a582da832b840037f8d1afb8f076a
Liby Lee <liby99@icloud.com>
2018-01-29 20:54:02 -0800

    Add Wasp and modified test files

---
+++ test/project2/tube_skin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include "viewer/SkinViewer.h"
+#include "object/skin/SkinParser.h"
+#include "object/skeleton/SkeletonParser.h"
+
+using namespace rotamina;
+
+int main () {
+    
+    // Initiate skel and skin
+    Skeleton tubeSkel;
+    Skin tubeSkin = Skin(tubeSkel);
+    SkeletonParser::loadSkeleton(tubeSkel, "../res/skeletons/tube.skel");
+    SkinParser::loadSkin(tubeSkin, "../res/skins/tube_smooth.skin");
+    
+    // Create the viewer
+    SkinViewer::createViewer(1600, 960, "CSE 169 Project 2 - Tube", tubeSkin, [&] (SkinViewer & v) {
+        
+    });
+}
---
+++ test/project2/wasp_skin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include "viewer/SkinViewer.h"
+#include "object/skin/SkinParser.h"
+#include "object/skeleton/SkeletonParser.h"
+
+using namespace rotamina;
+
+int main () {
+    
+    // Initiate skel and skin
+    Skeleton waspSkel;
+    Skin waspSkin = Skin(waspSkel);
+    SkeletonParser::loadSkeleton(waspSkel, "../res/skeletons/wasp.skel");
+    SkinParser::loadSkin(waspSkin, "../res/skins/wasp.skin");
+    
+    // Create the viewer
+    SkinViewer::createViewer(1600, 960, "CSE 169 Project 2 - Wasp", waspSkin, [&] (SkinViewer & v) {
+        
+    });
+}
