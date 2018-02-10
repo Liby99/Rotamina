d33d43fdb6bd24634956f3c5717f53f31ef76bb3
Liby Lee <liby99@icloud.com>
2018-02-10 14:42:20 -0800

    Initiated Character Viewer

--- src/extern/viewer/CharacterViewer.h
+++ src/extern/viewer/CharacterViewer.h
@@ -2,17 +2,17 @@
 // #define SKIN_VIEWER_H
 //
 // #include "viewer/SkeletonViewer.h"
-// #include "object/skin/Skin.h"
+// #include "object/character/Character.h"
 //
 // namespace rotamina {
-//     class SkinViewer : public SkeletonViewer {
+//     class CharacterViewer : public SkeletonViewer {
 //         protected:
 //             static const int CONTROL_PANEL_HEIGHT = 50;
 //             rotamina::Skin * skin;
 //             nanogui::Window * skinControlPanel;
 //         public:
-//             SkinViewer(int, int, std::string, Skin & skin);
-//             static void createViewer(int, int, std::string, Skin & skin, std::function<void(SkinViewer &)>);
+//             CharacterViewer(int, int, std::string, Skin & skin);
+//             static void createViewer(int, int, std::string, Character & character, std::function<void(CharacterViewer &)>);
 //         protected:
 //             virtual void showSkeleton();
 //             virtual void showSkin();
