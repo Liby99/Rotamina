803cf733daf0f8722eba7131fb5ef9550ac7392f
Liby Lee <liby99@icloud.com>
2018-02-10 14:35:58 -0800

    Commented Morph and Textured Skin

--- src/extern/object/skin/MorphSkin.h
+++ src/extern/object/skin/MorphSkin.h
@@ -1,21 +1,21 @@
-#ifndef MORPH_SKIN_H
-#define MORPH_SKIN_H
-
-#include "object/skin/TexturedSkin.h"
-
-namespace rotamina {
-    class MorphSkin : public TexturedSkin {
-        protected:
-            std::vector<float> weights;
-        public:
-            MorphSkin(Skeleton & skel);
-            void addWeight();
-            void setWeight(int, float);
-            std::vector<float> & getWeights();
-            float getWeight(int);
-        protected:
-            virtual void loadPosNorm(Eigen::MatrixXf &, Eigen::MatrixXf &);
-    };
-}
-
-#endif
+// #ifndef MORPH_SKIN_H
+// #define MORPH_SKIN_H
+//
+// #include "object/skin/TexturedSkin.h"
+//
+// namespace rotamina {
+//     class MorphSkin : public TexturedSkin {
+//         protected:
+//             std::vector<float> weights;
+//         public:
+//             MorphSkin(Skeleton & skel);
+//             void addWeight();
+//             void setWeight(int, float);
+//             std::vector<float> & getWeights();
+//             float getWeight(int);
+//         protected:
+//             virtual void loadPosNorm(Eigen::MatrixXf &, Eigen::MatrixXf &);
+//     };
+// }
+//
+// #endif
--- src/intern/object/skin/TexturedSkin.cpp
+++ src/intern/object/skin/TexturedSkin.cpp
@@ -1,46 +1,46 @@
-#include <iostream>
-
-#include "object/skin/TexturedSkin.h"
-
-using namespace rotamina;
-
-TexturedSkin::TexturedSkin(Skeleton & skel) : Skin(skel) {
-    renderTexture = true;
-}
-
-void TexturedSkin::loadTexture(const char * filename) {
-    texture.load(filename);
-}
-
-void TexturedSkin::setRenderTexture(bool render) {
-    this->renderTexture = render;
-}
-
-bool TexturedSkin::isRenderingTexture() {
-    return renderTexture;
-}
-
-void TexturedSkin::draw(rotamina::Shader & shader) {
-    
-    // If render texture then
-    if (this->renderTexture) {
-        
-        using namespace nanogui;
-        
-        // Set the uniform
-        shader.setTexture("skinTexture", texture);
-        
-        // Then update the tex coordinates
-        MatrixXf texCoords(2, skinVertexAmount());
-        for (int i = 0; i < skinVertexAmount(); i++) {
-            
-            // Load texture coords
-            Vector2f tc = vertices[i].getTexCoord();
-            texCoords.col(i) << tc[0], tc[1];
-        }
-        shader.uploadAttrib("texCoord", texCoords);
-    }
-    
-    // Let the skin to draw the rest
-    Skin::draw(shader);
-}
+// #include <iostream>
+//
+// #include "object/skin/TexturedSkin.h"
+//
+// using namespace rotamina;
+//
+// TexturedSkin::TexturedSkin(Skeleton & skel) : Skin(skel) {
+//     renderTexture = true;
+// }
+//
+// void TexturedSkin::loadTexture(const char * filename) {
+//     texture.load(filename);
+// }
+//
+// void TexturedSkin::setRenderTexture(bool render) {
+//     this->renderTexture = render;
+// }
+//
+// bool TexturedSkin::isRenderingTexture() {
+//     return renderTexture;
+// }
+//
+// void TexturedSkin::draw(rotamina::Shader & shader) {
+//
+//     // If render texture then
+//     if (this->renderTexture) {
+//
+//         using namespace nanogui;
+//
+//         // Set the uniform
+//         shader.setTexture("skinTexture", texture);
+//
+//         // Then update the tex coordinates
+//         MatrixXf texCoords(2, skinVertexAmount());
+//         for (int i = 0; i < skinVertexAmount(); i++) {
+//
+//             // Load texture coords
+//             Vector2f tc = vertices[i].getTexCoord();
+//             texCoords.col(i) << tc[0], tc[1];
+//         }
+//         shader.uploadAttrib("texCoord", texCoords);
+//     }
+//
+//     // Let the skin to draw the rest
+//     Skin::draw(shader);
+// }
