86a9bbb115bd70d59312b2ad7e83f3124d6bc4d6
Liby Lee <liby99@icloud.com>
2018-02-10 16:17:33 -0800

    Restored tests

--- test/project2/skin.cpp
+++ test/project2/skin.cpp
@@ -1,22 +1,13 @@
-// #include <iostream>
-// #include <vector>
-//
-// #include "object/skin/SkinParser.h"
-// #include "object/skeleton/SkeletonParser.h"
-//
-// using namespace rotamina;
-//
-// int main () {
-//     //
-//     // // Initiate skeleton and skin
-//     // Skeleton tubeSkel;
-//     // Skin tubeSkin = Skin(tubeSkel);
-//     //
-//     // // Load skel and skin from file
-//     // SkeletonParser::loadSkeleton(tubeSkel, "../res/skeletons/tube.skel");
-//     // SkinParser::loadSkin(tubeSkin, "../res/skins/tube_smooth.skin");
-//     //
-//     // tubeSkin.update();
-// }
+#include <iostream>
+#include <vector>
 
-int main () {}
+#include "object/character/CharacterParser.h"
+#include "object/skeleton/SkeletonParser.h"
+
+using namespace rotamina;
+
+int main () {
+    Character c;
+    CharacterParser::load(c, "../res/skeletons/tube.skel", "../res/skins/tube_smooth.skin");
+    c.update();
+}
