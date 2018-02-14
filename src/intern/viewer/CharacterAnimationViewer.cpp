5406bd936317ee5e331101d2e50837371b6ee7a7
Liby Lee <liby99@icloud.com>
2018-02-13 17:35:05 -0800

    Restored CMakeList from Damn CLion

--- src/intern/viewer/CharacterAnimationViewer.cpp
+++
@@ -1,7 +0,0 @@
-#include "viewer/CharacterAnimationViewer.h"
-
-using namespace rotamina;
-
-CharacterAnimationViewer::CharacterAnimationViewer(int w, int h, std::string name, CharacterAnimator & ca) : AnimationViewer(w, h, name, ca) {
-    this->characterAnimator = &ca;
-}
