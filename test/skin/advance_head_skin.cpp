143fc740de90095db61f94592736f45a1c5241fa
Liby Lee <liby99@icloud.com>
2018-02-26 17:01:34 -0800

    Fixed bug

--- test/skin/advance_head_skin.cpp
+++ test/skin/advance_head_skin.cpp
@@ -9,14 +9,14 @@ int main() {
     CharacterParser::load(c, "./res/skeletons/head.skel", "./res/skins/head_tex.skin");
     
     // Then load morphs into the skin
-    MorphParser::loadMorph(c.getAdvanceSkin(), "./res/morphs/head1.morph");
-    MorphParser::loadMorph(c.getAdvanceSkin(), "./res/morphs/head2.morph");
+    MorphParser::loadMorph(c.getSkin(), "./res/morphs/head1.morph");
+    MorphParser::loadMorph(c.getSkin(), "./res/morphs/head2.morph");
     
     c.update();
     
     AdvanceCharacterViewer::create(1600, 960, "CSE 169 Project 2 - Head - Morph & Textured", c, [&] (AdvanceCharacterViewer & v) {
     
         // After the viewer is ready, load the texture
-        c.getAdvanceSkin().loadTexture("./res/textures/head.bmp");
+        c.getSkin().loadTexture("./res/textures/head.bmp");
     });
 }
