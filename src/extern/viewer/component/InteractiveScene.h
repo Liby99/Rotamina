c549a8c321ac70ac3733d618e836fe7092e5bae3
Liby Lee <liby99@icloud.com>
2018-03-05 12:45:41 -0800

    Add Scene h

---
+++ src/extern/viewer/component/InteractiveScene.h
@@ -0,0 +1,18 @@
+#ifndef INTERACTIVE_SCENE_H
+#define INTERACTIVE_SCENE_H
+
+#include "viewer/component/Scene.h"
+
+namespace rotamina {
+    class InteractiveScene : public Scene {
+    protected:
+        Shader * selectionShader;
+        
+    public:
+        InteractiveScene(Widget *);
+        ~InteractiveScene();
+        virtual bool mouseButtonEvent(const Vector2i & p, int button, bool down, int modifiers);
+    };
+}
+
+#endif
---
+++ src/intern/viewer/component/InteractiveScene.cpp
@@ -0,0 +1,11 @@
+#include "viewer/component/InteractiveScene.h"
+
+using namespace rotamina;
+
+InteractiveScene::InteractiveScene(Widget * parent) : Scene(parent) {
+    
+}
+
+bool InteractiveScene::mouseButtonEvent(const Vector2i & p, int button, bool down, int modifiers) {
+    
+}
